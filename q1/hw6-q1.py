5721455e817e034e364b9721c873d05068beb0da
Vigneshwar Pesaru <77748485+Vigneshwar5072@users.noreply.github.com>
2021-11-13 16:12:55 -0500

    added q1.py file with screenshot

---
+++ q1/hw6-q1.py
@@ -0,0 +1,15 @@
+agile_status = ('Ready', 'InProgress', 'Blocked', 'Completed')
+agile_status[0] = 'Success'
+print(agile_status[0]) 
+#Answer-1
+#The built-in data structure name "Tuple" will  be used. 
+
+#Given that the agile status follows a specific order i.e.
+#1. Read
+#2. InProgress
+#3. Blocked
+#4. Completed
+
+#Since we know that from the definations tuples follows the following charcteristics:
+#1. A tuple is a collection which is ordered and unchangeable.
+#2. Tuples are unmmutable.
